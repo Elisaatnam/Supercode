739e4d07ae34ed092804b2d6b423e2e79ac360ac
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-04-26 15:38:38 +0200

    alternative solution lv 1-3 inner.HTML

--- 25-javascript-function/innerHTML/lv_1-3/assets/js/main.js
+++ 25-javascript-function/innerHTML/lv_1-3/assets/js/main.js
@@ -1,11 +1,21 @@
-let gallerySection = document.querySelector('#gallery');
+// let gallerySection = document.querySelector('#gallery');
 
-gallerySection.innerHTML = '<figure><img src="https://unsplash.it/250"><figcaption>Fig.1</figcaption></figure>';
-gallerySection.innerHTML += '<figure><img src="https://unsplash.it/250"><figcaption>Fig.2</figcaption></figure>';
-gallerySection.innerHTML += '<figure><img src="https://unsplash.it/250"><figcaption>Fig.3</figcaption></figure>';
+// gallerySection.innerHTML = '<figure><img src="https://unsplash.it/250"><figcaption>Fig.1</figcaption></figure>';
+// gallerySection.innerHTML += '<figure><img src="https://unsplash.it/250"><figcaption>Fig.2</figcaption></figure>';
+// gallerySection.innerHTML += '<figure><img src="https://unsplash.it/250"><figcaption>Fig.3</figcaption></figure>';
 
-gallerySection.style.height = '100vh';
-gallerySection.style.widht = '100vw';
-gallerySection.style.display = 'flex';
-gallerySection.style.justifyContent = 'center';
-gallerySection.style.alignItems = 'center';
+// gallerySection.style.height = '100vh';
+// gallerySection.style.widht = '100vw';
+// gallerySection.style.display = 'flex';
+// gallerySection.style.justifyContent = 'center';
+// gallerySection.style.alignItems = 'center';
+
+document.querySelector("#gallery").innerHTML = '<figure><img src="https://unsplash.it/250"><figcaption>Fig.1</figcaption></figure>';
+document.querySelector("#gallery").innerHTML += '<figure><img src="https://unsplash.it/250"><figcaption>Fig.2</figcaption></figure>';
+document.querySelector("#gallery").innerHTML += '<figure><img src="https://unsplash.it/250"><figcaption>Fig.3</figcaption></figure>';
+
+document.querySelector("#gallery").style.height = '100vh';
+document.querySelector("#gallery").style.widht = '100vw';
+document.querySelector("#gallery").style.display = 'flex';
+document.querySelector("#gallery").style.justifyContent = 'center';
+document.querySelector("#gallery").style.alignItems = 'center';
