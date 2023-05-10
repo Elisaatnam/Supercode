062bc22ca39b557b82faca5237f50127ee03c08d
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-05-10 15:36:22 +0200

    day 34 LOOPS (for, for of)

---
+++ 34-arrays-forLoop-forOfLoop/assets/js/main.js
@@ -0,0 +1,131 @@
+// Lv 1_1
+console.log("%c Lv 1_1", "color: red");
+
+for (let i = 1; i <= 10; i++) {
+	console.log(`Hello World ${i}`);
+}
+
+// Lv 1_2
+console.log("%c Lv 1_2", "color: red");
+
+let numArray = [];
+
+for (let i = 0; i <= 10; i++) {
+	numArray.push(i);
+}
+
+console.log(numArray);
+
+// Lv 1_4
+console.log("%c Lv 1_4", "color: red");
+
+let names = [
+	"Freddy",
+	"Steffen",
+	"Finn",
+	"Julia",
+	"Farid",
+	"Christian",
+	"Sergio",
+];
+
+for (let i of names) {
+	console.log(i);
+}
+
+// Lv 2_1
+console.log("%c Lv 2_1", "color: red");
+
+let imgArray = [];
+
+for (i = 1; i <= 100; i++) {
+	if (i < 10) {
+		imgArray.push(`image_00${i}`);
+	} else if (i < 100) {
+		imgArray.push(`image_0${i}`);
+	} else {
+		imgArray.push(`image_${i}`);
+	}
+}
+
+console.log(imgArray);
+console.log({ imgArray });
+
+// Lv 2_2
+console.log("%c Lv 2_2", "color: red");
+
+//output definieren
+const output = document.querySelector(".output");
+
+//loop() function ---> onsubmit
+function loop() {
+	//inputfeld auselsen
+	let num = Number(document.querySelector("#num").value);
+
+	// die anzahl der Buchstaben "O" ist zunaechst null/ leer
+	let anzahlO = "";
+
+	//fuegt je nach input Buchstaben hinzu, also bei null - null mal "O", bei 1 - 1 mal "O", bei 2 - 2 mal "O" usw....
+	for (let i = 0; i < num; i++) {
+		anzahlO += "O";
+	}
+	//Kontrolle ob es geklappt hat
+	//console.log(anzahlO.length);
+
+	//gibt L und P mit "O" aus, je nachdem welche zahl im input eingegeben wurde
+	output.innerHTML = `L${anzahlO}P`;
+}
+
+// Lv 3_2
+console.log("%c Lv 2_3", "color: red");
+
+let numArr = [5, 22, 15, 100, 55];
+const output2 = document.querySelector(".output2");
+
+for (let i = 0; i < numArr.length; i++) {
+	for (let j = 2; j < numArr[i]; j++) {
+		//console.log(numArr[i]);
+		//console.log(j);
+		if (numArr[i] % j == 0) {
+			output2.innerHTML += `Die zahl ${
+				numArr[i]
+			} ist teilbar durch ${j}! Das Ergenis ist ${numArr[i] / j} <br> `;
+		}
+	}
+}
+
+// Lv 3_3
+console.log("%c Lv 3_3", "color: red");
+
+//output definieren
+const output3 = document.querySelector(".output3");
+
+//loop() function ---> onsubmit
+function loop2() {
+	//inputfeld auselsen
+	let num2 = Number(document.querySelector("#num2").value);
+
+	// die anzahl der Buchstaben "O" ist zunaechst null/ leer
+	let anzahlO = "";
+
+	//fuegt je nach input Buchstaben hinzu, also bei null - null mal "O", bei 1 - 1 mal "O", bei 2 - 2 mal "O" usw....
+	if (num2 === 0) {
+		output3.innerHTML = "ERROR";
+	} else {
+		for (let i = 0; i < num2; i++) {
+			//wenn input nummer gerade dann nur kleines "o"
+			if (num2 % 2 === 0) {
+				anzahlO += "o";
+			}
+			//wenn input nummer ungerade dann "o" und "O" im Wechsel
+			else {
+				if (i % 2 === 0) {
+					anzahlO += "o";
+				} else {
+					anzahlO += "O";
+				}
+			}
+		}
+		output3.innerHTML = `L${anzahlO}P`;
+	}
+}
