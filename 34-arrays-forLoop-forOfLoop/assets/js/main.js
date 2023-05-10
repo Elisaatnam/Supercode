fd5138f334fb3fae578a42bfb3eb67089e1f9e8f
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-05-10 15:53:10 +0200

    added comments

--- 34-arrays-forLoop-forOfLoop/assets/js/main.js
+++ 34-arrays-forLoop-forOfLoop/assets/js/main.js
@@ -82,11 +82,16 @@ console.log("%c Lv 2_3", "color: red");
 let numArr = [5, 22, 15, 100, 55];
 const output2 = document.querySelector(".output2");
 
+//nested loop
+//zuerst der Loop fuer die length des Arrays in diesem Falle 5 durchlaeufe = also fuer jede Zahl des Arrays ein Loop
 for (let i = 0; i < numArr.length; i++) {
+	//nun wird fuer jede Zahl des Arrays geschaut, durch welche Zahl sie teilbar ist (j = 2 weil nicht geschaut werden soll ob die Zahl  durch 1 teilbar ist & j < numArr[i], weil nicht geschaut werden soll ob die Zahl  durch sich selbst teilbar ist)
 	for (let j = 2; j < numArr[i]; j++) {
 		//console.log(numArr[i]);
 		//console.log(j);
-		if (numArr[i] % j == 0) {
+
+		// wenn numArr[i] durch eine Zahl (j) teilbar ist, dann wird folgendes im output2.innerHTML ausgegeben
+		if (numArr[i] % j === 0) {
 			output2.innerHTML += `Die zahl ${
 				numArr[i]
 			} ist teilbar durch ${j}! Das Ergenis ist ${numArr[i] / j} <br> `;
@@ -100,7 +105,7 @@ console.log("%c Lv 3_3", "color: red");
 //output definieren
 const output3 = document.querySelector(".output3");
 
-//loop() function ---> onsubmit
+//loop2() function ---> onsubmit
 function loop2() {
 	//inputfeld auselsen
 	let num2 = Number(document.querySelector("#num2").value);
@@ -108,10 +113,13 @@ function loop2() {
 	// die anzahl der Buchstaben "O" ist zunaechst null/ leer
 	let anzahlO = "";
 
-	//fuegt je nach input Buchstaben hinzu, also bei null - null mal "O", bei 1 - 1 mal "O", bei 2 - 2 mal "O" usw....
+	// wenn input nummer gleich null dann wird "ERROR" ausgegeben
 	if (num2 === 0) {
 		output3.innerHTML = "ERROR";
-	} else {
+	}
+
+	//fuegt je nach input Buchstaben hinzu, also bei null - null mal "O", bei 1 - 1 mal "O", bei 2 - 2 mal "O" usw....
+	else {
 		for (let i = 0; i < num2; i++) {
 			//wenn input nummer gerade dann nur kleines "o"
 			if (num2 % 2 === 0) {
